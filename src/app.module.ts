7c6d3c595c8bb7286c731f8ab296baa948e5bbcb
Rushab Ambre <rushab_a@khika.com>
2022-06-21 16:29:25 +0530

    added env config and mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
-
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    envFilePath: '.dev.env',
+  }),
+  MongooseModule.forRoot(process.env.MONGO_URL+process.env.DB_NAME)
+],
   controllers: [AppController, ],
   providers: [AppService, ],
 })
